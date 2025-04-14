d9088f9891a73404ad4e1c54dbdc591be8939890
root <root@localhost.localdomain>
2025-04-14 10:33:17 -0300

    ✅ Agregado tablero completo al admin para edición

--- planilla/admin.py
+++ planilla/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
-#from .models import ReglaAcceso
-from .models import Tablero
-
-
-
+from .models import Tablero, PerfilUsuario
 
 @admin.register(Tablero)
 class TableroAdmin(admin.ModelAdmin):
-    list_display = ('eje_estrategico', 'indicador', 'avance', 'nivel', 'accion', 'responsable')
-    list_filter = ('nivel', 'avance')
-    search_fields = ('indicador', 'objetivo_estrategico')
+    list_display = (
+        'eje_estrategico',
+        'objetivo_estrategico',
+        'indicador',
+        'meta_2025',
+        'avance',
+        'nivel',
+        'accion',
+        'responsable',
+    )
+    list_filter = ('eje_estrategico', 'nivel', 'responsable')
+    search_fields = ('indicador', 'objetivo_estrategico', 'accion')
+    ordering = ('eje_estrategico', 'objetivo_estrategico')
+
+@admin.register(PerfilUsuario)
+class PerfilUsuarioAdmin(admin.ModelAdmin):
+    list_display = ('user', 'responsable')
+    search_fields = ('user__username', 'responsable')
