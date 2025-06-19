fb387f27701acf2a83fc0518f8b99ba55def9cf2
root <root@pei.conatel.gov.py>
2025-06-19 15:35:55 -0300

    Agregado static() para servir archivos multimedia en entorno de desarrollo

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -18,9 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
+from django.conf import settings
+from django.conf.urls.static import static  # 👈 Import requerido para servir media
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('planilla.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+
+# ✅ Agregado para servir archivos de evidencias (media)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
